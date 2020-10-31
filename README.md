d9724a9041febf21cf67dc27ed48e81b5a22dcd2
Giampaolo Spagoni <giampaolo.spagoni@infor.com>
2020-10-31 20:35:48 +0100

    first version of package

--- README.md
+++ README.md
@@ -1,1 +1,84 @@
 # inforapipackage
+
+this package is meant to handle ionapi file of type backend service
+
+# Installation
+
+` npm install inforapipackege -D `
+
+it is composed by 4 functions
+
+- InitializeApiFile(filename)
+- Status
+- Message
+- RetunrTokenObj
+
+<hr>
+
+## InitializeApiFile(filename)
+this function analyzes the filename checking if it is of type ionapi and 
+if it is of type backend service
+if all checks are passed, set the Status to 'OK' and the Message with the text
+'All checks passed', on the contrary if there are errors, it set the Status
+to 'KO' and the Message to the error message
+
+```
+const infor = require('inforapipackage');
+......
+app.listen(3000, () => {
+    infor.InitializeApiFile('./filename.ionapi');
+    if(infor.Status() == 'KO'){
+        console.log(infor.Message())
+        exit(1)
+    }else{
+        console.log('Server running at http://localhost:3000')
+    }
+})
+```
+
+## Status
+this function return the status setted by the InitializeApiFile function
+
+values:
+- OK : all checks passed
+- KO : errors
+
+```
+    if(infor.Status() == 'KO'){
+        console.log(infor.Message())
+        exit(1)
+    }else{
+        console.log('Server running at http://localhost:3000')
+    }
+```
+
+## Message
+this function print the message setted by the InitializeApiFile function
+if the check of the file fail, then the Message function print the error message
+if the check of the file pass, then the Message function print the text 'All checks passed'
+
+```
+    console.log(infor.Message())
+```
+
+## RetunrTokenObj
+this function return an object that contains the token
+you can use in an async function or as a promise
+
+```
+async
+
+app.get('/', async (req,res) =>{
+    const RTO = await infor.ReturnTokenObj();
+    const token = RTO.access_token
+    makeApiCall(token...)
+})
+
+promise
+
+    infor.ReturnTokenObj()
+    .then(rto => console.log(rto.access_token))
+    .catch(e => console.error(e))
+```
+
+![image](https://user-images.githubusercontent.com/22134155/97788174-39f11c80-1bb7-11eb-8fd7-725c48b5b87d.png)
--- index.js
+++ index.js
@@ -13,20 +13,21 @@
  * 
  */
 const fs = require('fs')
-const path = require('path'); 
+const request = require('request')
+const base64 = require('base-64');
+
 
 var ionapi = "";
 var retObj = {
-    "ionapi":"",
     "status":"",
     "message":""
 }
 
-exports.initializeApiFile = function()
+exports.InitializeApiFile = function(ionapiFile)
 {
 
     try{
-        this.ionapi = fs.readFileSync(path.join(__dirname,"ionapi",ionapiFile), {encoding: 'utf8'})
+        this.ionapi = fs.readFileSync(ionapiFile, {encoding: 'utf8'})
         if(!this.ionapi.includes('ti') && !this.ionapi.includes('cn') && !this.ionapi.includes('ci') && !this.ionapi.includes('cs'))
         {
             msgText = "Wrong file type. Select a correct <file>.ionapi file..."
@@ -60,5 +61,89 @@ exports.initializeApiFile = function()
     retObj.status = "OK";
     retObj.message = "All checks passed";
     ionapi = this.ionapi;
-    return retObj;
+    //return retObj;
 }
+
+exports.Status = function() {
+    return retObj.status;
+}
+
+exports.Message = function() {
+    return retObj.message;
+}
+
+
+/**
+ * 
+ *  Function to get the TOKEN
+ */
+function requestPromise(options) {
+    return new Promise(function promisifiedRequest(resolve, reject) {
+        request(options, function readResponse(err, res, body) {
+            //console.log(from + ":--->    " + body);
+            if (err) {
+                return reject(err);
+            }
+            if (res.statusCode === 200 || res.statusCode === 201) {
+                    return resolve(body);
+            }
+            return reject(new Error(`IONAPI responded with status: ${res.statusCode}`));
+        });
+    });
+ }
+ 
+ 
+ async function getResponseFromAPI(options)
+ {
+   let result = "";
+    
+   try{
+        const response = await requestPromise(options)
+      if (!options.mess){
+         const responseJson = await JSON.parse(response)
+         result = responseJson		    			
+      }else{
+         result = response
+      }
+   } catch(e) {
+      result = e
+   }
+   return result
+ }
+ 
+ //async function returnTokenObj(ionapi)
+ exports.ReturnTokenObj = async function ()
+ {
+    const ci = ionapi.ci;
+    const cs = ionapi.cs;
+    const combine = ci + ":" + cs
+    const cicsbase64 = base64.encode(`${combine}`);
+    
+    const saak = encodeURIComponent(ionapi.saak);
+    const sask = encodeURIComponent(ionapi.sask);
+ 
+    const options1 = {
+       method: 'POST',
+       url: ionapi.pu + ionapi.ot, //'https://mingle-sso.inforcloudsuite.com:443/MINGLETEST1_DEV/as/token.oauth2',
+       headers: {
+           'cache-control': 'no-cache',
+           'authorization': `Basic ${cicsbase64}`,
+           'content-type': 'application/x-www-form-urlencoded'
+       },
+       form: false,
+       body: `grant_type=password&username=${saak}&password=${sask}`,
+       rejectUnauthorized: false  
+   };
+ 
+ //  console.log("======================================================================")
+ //  console.log("GETTING TOKEN...")
+   let getToken = await getResponseFromAPI(options1)
+ //  console.log("======================================================================")
+ //  console.log(getToken)
+ //  console.log("======================================================================")
+ 
+   return getToken;
+ }
+ 
+ 
+ 
